1a3214aaf7f3079d3af6fc20ec3d774ed254254a
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-04-23 03:16:14 +0700

    commit

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,16 +1,19 @@
-#include "publisher_subscriber_server_client/subscriber.hpp"
+#include "ros/ros.h"
+#include "publisher_subscriber_server_client/Message.h"
 
 void callback(const publisher_subscriber_server_client::Message::ConstPtr& msg)
 {
-  
+    ROS_INFO("Received message: %ld", msg->num);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "subscriber");
-  ros::NodeHandle nh;
+    ros::init(argc, argv, "subscriber");
+    ros::NodeHandle nh;
 
-  
+    ros::Subscriber sub = nh.subscribe("message_topic", 1000, callback);
 
-  return 0;
+    ros::spin();
+
+    return 0;
 }
